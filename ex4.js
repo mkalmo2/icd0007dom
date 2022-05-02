7d7836324dbb4f53920875833f3d828b49215680
Märt Kalmo <mart.kalmo@gmail.com>
2022-05-02 09:31:06 +0300

    cleanup

--- ex4.js
+++ ex4.js
@@ -1,30 +1,22 @@
-(function () {
-    'use strict';
+import Item from "./Item.js";
 
-    let ul = document.getElementById('list');
-    let button = document.getElementById('button1');
-    let input = document.getElementById('input1');
+const ul = document.getElementById('list');
+const button = document.getElementById('button1');
+const input = document.getElementById('input1');
 
-    let counter = 1;
-    let items = [new Item(counter++, 'item 1'), new Item(counter++, 'item 2')];
+let counter = 1;
+let items = [new Item(counter++, 'item 1'), new Item(counter++, 'item 2')];
 
-    render(); // call render function after page is loaded.
+render(); // call render function after page is loaded.
 
-    function render() {
+function render() {
 
-        // create dom element for each item in items list.
+    // create dom element for each item in items list.
 
-    }
+}
 
-    function addLi(item) {
-        let li = document.createElement('li');
-        li.innerText = item.text;
-        ul.appendChild(li);
-    }
-
-    function Item(id, text) {
-        this.id = id;
-        this.text = text;
-    }
-
-})();
+function addLi(item) {
+    const li = document.createElement('li');
+    li.innerText = item.text;
+    ul.appendChild(li);
+}
